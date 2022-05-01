0b064b41a4b2d2225b5fb3e6910dddda4eb7d5c1
barek <bareketatia@gmail.com>
2022-05-01 20:21:49 +0300

    add more classes

---
+++ src/Constants.java
@@ -0,0 +1,24 @@
+public class Constants {
+    public static final int PERSON_DATA_PANEL_WIDTH=800;
+    public static final int PERSON_DATA_PANEL_HEIGHT=700;
+    public static final int TEXT_FILED_COLUMNS=5;
+    public static final int GENDER_PANEL_WIDTH= 50;
+    public static final int GENDER_PANEL_HEIGHT= 50;
+    public static final int GENERAL_PANEL_WIDTH= 2000;
+    public static final int  GENERAL_PANEL_HEIGHT= 1000;
+    public static final int USER_HEIGHT_PANEL_WIDTH=100;
+    public static final int USER_HEIGHT_PANEL_HEIGHT=100;
+    public static final int BODY_STRUCTURE_PANEL_WIDTH=300;
+    public static final int BODY_STRUCTURE_PANEL_HEIGHT=100;
+
+
+
+
+
+
+
+
+
+
+
+}
